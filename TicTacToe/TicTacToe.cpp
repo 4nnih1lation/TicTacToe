3c956556723a3ec63a73db8814c7fa97d1f4ac80
Ilya <ilya.chirkovskiy@gmail.com>
2022-06-15 18:59:59 +0300

    Fixed bag in win_draw_lose

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 using namespace std;
 using namespace chrono;
-const short sz_f = 3;
 
 int getRandom(int min, int max) {
     const static auto seed = system_clock::now().time_since_epoch().count();
@@ -33,7 +32,7 @@ struct game_coord{
 
 struct game{
     const short size_field = 3;
-    symbols** field = {nullptr};
+    symbols** field {nullptr};
     game_progress status{ in_progress };
     symbols player1;
     symbols player2;
@@ -46,15 +45,13 @@ void __fastcall clear_field() {
 
 void __fastcall init_game(game& g) {
     g.field = new symbols* [g.size_field];
-    for (short i = 0; i < g.size_field; i++) {
+    for (short i = 0; i < g.size_field; i++) 
         g.field[i] = new symbols [g.size_field];
-    }
-    for (short x = 0; x < g.size_field; x++) {
-        for (short y = 0; y < g.size_field; y++) {
+    
+    for (short x = 0; x < g.size_field; x++) 
+        for (short y = 0; y < g.size_field; y++) 
             g.field[x][y] = s_empty;
-        }
-    }
-
+        
     if (getRandom(0, 10) > 5) {
         g.player1 = s_cross;
         g.player2 = s_zero;
@@ -69,24 +66,25 @@ void __fastcall init_game(game& g) {
 
 void __fastcall print_field(const game& g) {
     cout << "    ";
-    for (short x = 0; x < g.size_field; x++) {
+    for (short x = 0; x < g.size_field; x++) 
         cout << x + 1 << " | ";
-    }
+    
     cout << endl;
+    
     for (short y = 0; y < g.size_field; y++) {
         cout << y + 1 << " | ";
-        for (short x = 0; x < g.size_field; x++) {
+        for (short x = 0; x < g.size_field; x++)
             cout << g.field[x][y] << " | ";
-        }
+        
         cout << endl;
    }
     cout << g.player1 << " - player 1\n" << g.player2 << " - player2\n";
 }
 
 void __fastcall deinit_game(game& g) {
-    for (short i = 0; i < g.size_field; i++) {
+    for (short i = 0; i < g.size_field; i++) 
         delete[] g.field[i];
-    }
+    
     delete[] g.field;
     g.field = nullptr;
 }
@@ -94,9 +92,9 @@ void __fastcall deinit_game(game& g) {
 game_coord __fastcall get_coord_player1(game& g) {
     game_coord c{ 0 };
     do {
-        cout << "Enter X";
+        cout << "Enter X: ";
         cin >> c.x;
-        cout << "Enter Y";
+        cout << "Enter Y: ";
         cin >> c.y;
         c.x--;
         c.y--;
@@ -107,9 +105,9 @@ game_coord __fastcall get_coord_player1(game& g) {
 game_coord __fastcall get_coord_player2(game& g) {
     game_coord c{ 0 };
     do {
-        cout << "Enter X";
+        cout << "Enter X: ";
         cin >> c.x;
-        cout << "Enter Y";
+        cout << "Enter Y: ";
         cin >> c.y;
         c.x--;
         c.y--;
@@ -119,79 +117,63 @@ game_coord __fastcall get_coord_player2(game& g) {
 
 game_progress __fastcall win_lose_draw(game& g) {
     for (short y = 0; y < g.size_field; y++) {
-        if(g.field[0][y] == g.field[1][y] && g.field[0][y] == g.field[2][y])
-            if (g.field[0][y] == g.player1) {
+        if (g.field[0][y] == g.field[1][y] && g.field[0][y] == g.field[2][y]) {
+            if (g.field[0][y] == g.player1)
                 return player1_won;
-            }
-        if (g.field[0][y] == g.player2) {
-            return player2_won;
+            if (g.field[0][y] == g.player2)
+                return player2_won;
         }
     }
     for (short x = 0; x < g.size_field; x++){
         if (g.field[x][0] == g.field[x][1] && g.field[x][0] == g.field[x][2]) {
-            if (g.field[x][0] == g.player1) {
+            if (g.field[x][0] == g.player1)
                 return player1_won;
-            }
-            if (g.field[x][0] == g.player2) {
+            if (g.field[x][0] == g.player2)
                 return player2_won;
-            }
         }
     }
     if (g.field[0][0] == g.field[1][1] && g.field[0][0] == g.field[2][2]) {
-        if (g.field[0][0] == g.player1) {
+        if (g.field[0][0] == g.player1)
             return player1_won;
-        }
-        if (g.field[0][0] == g.player2){
+        if (g.field[0][0] == g.player2)
             return player2_won;
-        }
     }
-    if (g.field[2][0] == g.field[1][1] && g.field[2][0] == g.field[0][2]) {
-        if (g.field[2][0] == g.player1) {
+    if (g.field[0][2] == g.field[1][1] && g.field[0][2] == g.field[2][0]) {
+        if (g.field[2][0] == g.player1) 
             return player1_won;
-        }
-        if (g.field[2][0] == g.player2) {
-            return player2_won;
-        }
+        if (g.field[2][0] == g.player2) 
+            return player2_won;       
     }
-    bool Draw{ true };
+    bool drw{ true };
     for (short x = 0; x < g.size_field; x++) {
         for (short y = 0; y < g.size_field; y++) {
             if (g.field[x][y] == s_empty) {
-                Draw = false;
+                drw = false;
                 break;
             }
         }
-        if (Draw == false) {
-            break;
-        }
+        if (drw == false) 
+            break;    
     }
-    if (Draw) {
+    if (drw) 
         return draw;
-    }
+    
     return in_progress;
 }
 
 void __fastcall results(const game& g) //Подводит результаты по игре
 {
     if (g.status == player1_won)
-    {
         std::cout << "Player 1 won\n";
-    }
 
     if (g.status == player2_won)
-    {
         std::cout << "Player 2 won\n";
-    }
-
+    
     if (g.status == draw)
-    {
         std::cout << "Draw\n";
-    }
-
 }
 
-int main()
-{
+int main(void){
     game g;
     init_game(g);
     print_field(g);
@@ -212,5 +194,4 @@ int main()
 
     deinit_game(g);
     results(g);
-    return 0;
 }
